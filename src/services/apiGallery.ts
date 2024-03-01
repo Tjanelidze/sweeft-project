703de03da4137ccf4ea4fd9d44b47cd6c81218f6
Tedo <tedojanelidze7@gmail.com>
2024-03-01 14:07:18 +0400

    added env

--- src/services/apiGallery.ts
+++ src/services/apiGallery.ts
@@ -1,9 +1,11 @@
+const apiKey = import.meta.env.VITE_ACCESS_KEY;
+
 export default async function apiGallery(searchQuery: string) {
   const res = await fetch(
     `https://api.unsplash.com/search/photos?page=1&query=${searchQuery}`,
     {
       headers: {
-        Authorization: 'Client-ID NrlOnvgA97lWUqc5avohXe8wZ6IO2_B8SDfRp8Jb5eg',
+        Authorization: `Client-ID ${apiKey}`,
       },
     }
   );
