3070ec8218905fb38ec7109cea6685c995cb51af
Tedo <tedojanelidze7@gmail.com>
2024-03-03 15:55:23 +0400

    added localstorage for hisotry

--- src/features/gallery/SearchBar.tsx
+++ src/features/gallery/SearchBar.tsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 import useGallery from './useGallery';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useImageContext } from '../../context/ImageContext';
-import { UnsplashImage } from '../../context/interfaces';
-import { useSearchParams } from 'react-router-dom';
 
 const Input = styled.input`
   border: 1px solid #777;
@@ -20,10 +18,34 @@ export default function SearchBar() {
   const { updateImages, setIsLoading, searchQuery, setSearchQuery } =
     useImageContext();
   const { isLoading, images } = useGallery(searchQuery);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setSearchQuery(value);
+  };
+
+  const saveToLocalStorage = () => {
+    if (searchQuery.trim() !== '') {
+      let searchHistory: string[] = [];
+      const storedSearchHistory = localStorage.getItem('searchHistory');
+      if (storedSearchHistory !== null) {
+        searchHistory = JSON.parse(storedSearchHistory);
+      }
+
+      searchHistory.push(searchQuery.trim());
+
+      localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+    }
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      // Trigger search when Enter key is pressed
+      const value = (event.target as HTMLInputElement).value;
+      setSearchQuery(value);
+      saveToLocalStorage();
+    }
   };
 
   useEffect(() => {
@@ -44,10 +66,20 @@ export default function SearchBar() {
     setIsLoading(isLoading);
   }, [isLoading, images]);
 
+  useEffect(() => {
+    inputRef.current?.addEventListener('blur', saveToLocalStorage);
+
+    return () => {
+      inputRef.current?.removeEventListener('blur', saveToLocalStorage);
+    };
+  }, [searchQuery]);
+
   return (
     <Input
+      ref={inputRef}
       value={searchQuery}
       onChange={handleInputChange}
+      onKeyDown={handleKeyPress}
       placeholder="search for free photos"
     />
   );
